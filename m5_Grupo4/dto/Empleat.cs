8b5432ebb0edcb9faa59ac3755273e97ee08c71e
cathvafc <miguelangelgomezlope@gmail.com>
2021-01-08 10:32:50 +0100

    Milestone 1

--- m5_Grupo4/dto/Empleat.cs
+++ m5_Grupo4/dto/Empleat.cs
@@ -6,5 +6,50 @@ namespace m5_Grupo4
 {
     class Empleat
     {
+        string tipoEmpleado = "";
+        double sueldo = 0;
+
+        public Empleat()
+        {
+
+        }
+
+        private void AjustarSueldo()
+        {
+            if (tipoEmpleado == "Manager")
+            {
+                sueldo *= 1.1; // Se aumenta el sueldo un 10% 
+               
+            }
+            else if (tipoEmpleado == "Boss")
+            {
+                sueldo *= 1.5; // Se aumenta el sueldo un 50% 
+
+            }
+            else if (tipoEmpleado == "Employee")
+            {
+                sueldo *= 0.85; // Se reduce el sueldo un 15% 
+
+            }
+            else if (tipoEmpleado == "Volunteer")
+            {
+                sueldo *= 0; // Se reduce el sueldo un 100% 
+            }
+            else
+            {
+                Console.WriteLine("Has introducido {0} que es un tipo de empleado erroneo.", tipoEmpleado);
+            }
+        }
+
+        public void Pedir()
+        {
+            Console.WriteLine("Introduce tu tipo de empleado:");
+            tipoEmpleado = Console.ReadLine();
+            Console.WriteLine("Introduce el sueldo:");
+            sueldo = Convert.ToDouble(Console.ReadLine());
+
+            AjustarSueldo();
+
+        }
     }
 }
